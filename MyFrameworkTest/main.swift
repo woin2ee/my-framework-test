abdce9c726c665454ae9e994d2faec366f176571
Jaewon Yun <woin2ee@gmail.com>
2023-02-23 20:31:23 +0900

    프레임워크 업데이트 & 서브클래싱

--- MyFrameworkTest/main.swift
+++ MyFrameworkTest/main.swift
@@ -8,8 +8,28 @@
 import Foundation
 import MyFramework
 
-let greetingMessage = GreetingGenerator.generate()
+let greetingMessage = GreetingGenerator().generate()
 
 print(greetingMessage + "!!")
 
 print("!!!")
+
+
+
+
+
+class KoreanGreetingGenerator: GreetingGenerator {
+    
+    override init() { // public 인데 어떻게 override?
+        print("init")
+    }
+    
+    override func generate() -> String {
+        return "반갑습니다."
+    }
+}
+
+
+let koreanGreetingMessage = KoreanGreetingGenerator().generate()
+
+print(koreanGreetingMessage)
